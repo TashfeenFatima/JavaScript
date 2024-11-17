cdaf2670bf41cf3e7cd06d68fd853f5eb004bfc7
TashfeenFatima <135822499+TashfeenFatima@users.noreply.github.com>
2024-11-18 02:25:24 +0530

    Add files via upload

---
+++ basics/03_StringConversion.js
@@ -0,0 +1,41 @@
+
+//console.log(2+2);    Addition operation
+//console.log(2-2);    Subtraction operation
+//console.log(2*2);    Multiplication operation
+//console.log(2**2);   Power operation  (works as 2 power 2)
+//console.log(2/2);    Division Operation
+//console.log(2%3);    Modulo operation
+
+//let str1 = "hello"
+//let str2 = " hitesh"
+
+/**
+ let str3 = str1 + str2
+console.log(str3);
+ */
+//But the prb occurs here
+console.log("1" + 2); //outputs 12
+console.log(1 + "2"); //outputs 12
+console.log("1" + 2 + 2); //outputs 122
+console.log(1 + 2 + "2"); //outputs 32
+console.log(+true); //outputs 1
+console.log(true+1); //outputs 2
+console.log(+""); //outputs 0
+
+
+//such code is not reviewed 😢
+
+let n1, n2, n3
+n1 = n2 = n3 = 2 + 2
+console.log(n1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined >0);
+console.log(undefined==0);
+console.log(undefined<0);
+
+
+
